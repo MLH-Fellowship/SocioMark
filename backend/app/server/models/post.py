3ce82490d0fa6905b92c924016f0b921f9f89517
Aitik Gupta <aitikgupta@gmail.com>
2021-03-26 05:17:53 +0530

    Add database schema

---
+++ backend/app/server/models/post.py
@@ -0,0 +1,50 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class PostSchema(BaseModel):
+    user_id: str = Field(...)
+    report_counter: int = Field(..., ge=0)
+    image: str = Field(...)
+    description: str = Field(None)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "user_id": "user_id",
+                "report_counter": 0,
+                "image": "url_of_image",
+                "description": "This is a post by <user_id>"
+            }
+        }
+
+
+class UpdatePostModel(BaseModel):
+    post_id: Optional[str]
+    user_id: Optional[str]
+    report_counter: Optional[int]
+    image: Optional[str]
+    description: Optional[str]
+
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "user_id": "user_id",
+                "report_counter": 0,
+                "image": "url_of_image",
+                "description": "This is a post by <user_id>"
+            }
+        }
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
---
+++ backend/app/server/models/user.py
@@ -0,0 +1,49 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+    profile_picture: str = Field(None)
+    description: str = Field(None)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "jdoe@x.edu.ng",
+                "password": "encrypted_password",
+                "profile_picture": "url_of_image",
+                "description": "This is John Doe"
+            }
+        }
+
+
+class UpdateUserModel(BaseModel):
+    email: Optional[EmailStr]
+    password: Optional[str]
+    profile_picture: Optional[str]
+    description: Optional[str]
+
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "jdoe@x.edu.ng",
+                "password": "encrypted_password",
+                "profile_picture": "url_of_image",
+                "description": "This is John Doe"
+            }
+        }
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
