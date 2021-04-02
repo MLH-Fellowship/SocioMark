4d32677a42bc290bdd7f05942a79d6f0b43694e7
bodhisha <bodhisha@gmail.com>
2021-04-03 03:57:42 +0530

    add email and password validation functions

---
+++ frontend/src/Components/Utils/validations.js
@@ -0,0 +1,9 @@
+export const validateEmailAddress = (email) => {
+  const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(email);
+};
+
+export const validatePassword = (password) => {
+  const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@!%*?%#`^()_=+{}[|\\\];:'"<>,./?~&])[A-Za-z\d$@!%*?%#`^()_=+{}[|\\\];:'"<>,./?~&]{8,49}/;
+  return pattern.test(password);
+};
