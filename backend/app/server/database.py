1e2682e9384f2bedf2d9a756ae7ad6f771e27dd1
Deepak Agrawal <debug.career@gmail.com>
2021-04-04 05:07:48 +0530

    Configured Mongo URL

--- backend/app/server/database.py
+++ backend/app/server/database.py
@@ -3,7 +3,7 @@ from dotenv import dotenv_values, find_dotenv
 
 config = dotenv_values(find_dotenv())
 
-MONGO_DETAILS = config["MONGODB_URL"]
+MONGO_DETAILS = "mongodb+srv://sociomark:sociomark@userdb.7mwfe.mongodb.net/users?retryWrites=true&w=majority"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
