c65de98aa040e51cf52442b27c238c96c591c374
Deepak Agrawal <debug.career@gmail.com>
2021-04-04 05:28:57 +0530

    Configured all .env urls

--- backend/app/server/database.py
+++ backend/app/server/database.py
@@ -1,9 +1,9 @@
 import motor.motor_asyncio
 from dotenv import dotenv_values, find_dotenv
-
+import os
 config = dotenv_values(find_dotenv())
 
-MONGO_DETAILS = "mongodb+srv://sociomark:sociomark@userdb.7mwfe.mongodb.net/users?retryWrites=true&w=majority"
+MONGO_DETAILS = os.environ.get("MONGODB_URL")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
