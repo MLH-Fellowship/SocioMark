41142b61df74379af37379e38e2ed7cfd491b6bc
Aitik Gupta <aitikgupta@gmail.com>
2021-04-05 04:11:58 +0530

    Add toasts container div globally, with styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import PublicRouter from "./Router/PublicRouter";
 import PrivateRouter from "./Router/PrivateRouter";
+import { ToastContainer } from 'react-toastify';
+import { injectStyle } from "react-toastify/dist/inject-style";
+
 
 function App() {
   const access = localStorage.getItem("access_token");
-  return <div>{access ? <PrivateRouter /> : <PublicRouter />}</div>;
+  injectStyle();
+
+  return <div>
+    {access ? <PrivateRouter /> : <PublicRouter />}
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
+  </div>;
 }
 
 export default App;
