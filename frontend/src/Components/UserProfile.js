f0aced4d444daf54ee92c8a8c70dfec51c755d5f
Aitik Gupta <aitikgupta@gmail.com>
2021-04-08 00:56:12 +0530

    Fix user, token for Feed

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -2,13 +2,11 @@ import React, { useContext } from "react";
 import { AuthContext } from "../Context/AuthContext";
 
 export default function UserFeed() {
-  const { value1, value2 } = useContext(AuthContext);
-  const [user] = value1;
-  const [access] = value2;
+  const { user, token } = useContext(AuthContext);
   return (
     <div>
       <div className="text-2xl font-bold">
-        Hey {user.name} {access}
+        Hey {user[0].name} {token[0]}
       </div>
     </div>
   );
