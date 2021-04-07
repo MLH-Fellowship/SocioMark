4cc3a5af75669e5a10a8ef9d9cb8b25b3883af02
Aitik Gupta <aitikgupta@gmail.com>
2021-04-08 03:59:59 +0530

    Implement like logic in frontend

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -2,12 +2,13 @@ import React, { useState, useContext } from "react";
 import Comment from "./Comment";
 import CreateComment from "./CreateComment";
 import axios from "axios";
+import { A } from "hookrouter";
 import { toast } from "react-toastify";
 import { AuthContext } from "../../Context/AuthContext";
-import { POST_UNCOMMENT_URL } from "../../constants";
+import { POST_UNCOMMENT_URL, POST_LIKE_UNLIKE_URL } from "../../constants";
 
 export default function Post({ post_initializer }) {
-  const { token } = useContext(AuthContext);
+  const { user, token } = useContext(AuthContext);
   const [access] = token;
   const [post, setPost] = useState(post_initializer);
 
@@ -16,6 +17,46 @@ export default function Post({ post_initializer }) {
     new_post.comments.push(new_comment);
     setPost(new_post);
   };
+
+  const handleLike = (post_id) => {
+    console.log(post_id);
+    axios
+      .post(
+        POST_LIKE_UNLIKE_URL,
+        { post_id },
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer " + access,
+          },
+        }
+      )
+      .then((res) => {
+        toast.info(JSON.stringify(res.data.message));
+        let new_post = Object.assign({}, post);
+        if (res.data.data.to_delete === false) {
+          console.log("Deleting", res.data.data);
+          var filtered_likes = new_post.likes.filter(function (el) {
+            return el.like_id !== res.data.data.like_id;
+          });
+          new_post.likes = filtered_likes;
+        } else {
+          console.log("Adding", res.data.data);
+          new_post.likes.push(res.data.data);
+        }
+        console.log(new_post.likes);
+        setPost(new_post);
+      })
+      .catch(({ response }) => {
+        if (response) {
+          toast.error(JSON.stringify(response.data.detail));
+        }
+      });
+  };
+
+  function likesContainID(likes_list, user_id) {
+    return likes_list.some((el) => el.user_id === user_id);
+  }
 
   const handleDeleteComment = (comment_id) => {
     axios
@@ -58,15 +99,38 @@ export default function Post({ post_initializer }) {
               </h3>
             </div>
             <div className="flex gap-x-6">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                fill="currentColor"
-                className="bi bi-heart"
-                viewBox="0 0 16 16"
+              { post.likes.length }
+              <A
+                href="#"
+                onClick={() => {
+                  handleLike(post.post_id);
+                }}
               >
-                <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-              </svg>
+                {likesContainID(post.likes, user[0].user_id) ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    fill="red"
+                    class="bi bi-heart-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    fill="currentColor"
+                    className="bi bi-heart"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                  </svg>
+                )}
+              </A>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
