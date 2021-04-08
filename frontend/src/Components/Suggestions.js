bb1c2d14711bcf2af400b75ce09946eb20e10655
bodhisha <bodhisha@gmail.com>
2021-04-09 01:16:48 +0530

    add link to name and profile pic which routes to user profile

--- frontend/src/Components/Suggestions.js
+++ frontend/src/Components/Suggestions.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { A } from "hookrouter";
 
 export default function Suggestions({ user }) {
   return (
     <div className="sticky top-0 mt-4 px-2 ">
       <div className="flex justify-between">
         <div className="flex gap-x-1 sm:gap-x-2 items-center">
-          <img
-            className="rounded-full h-8 w-8 shadow"
-            src={user.profile_picture}
-          />
-          <div className="text-sm font-medium text-gray-900">{user.name}</div>
+          <A href={`/user/${user.user_id}`}>
+            <img
+              className="rounded-full h-8 w-8 shadow"
+              src={user.profile_picture}
+            />
+          </A>
+          <A href={`/user/${user.user_id}`}>
+            <div className="text-sm font-medium text-gray-900">{user.name}</div>
+          </A>
           <div className="flex gap-x-1 items-center bg-green-50 text-green-700 p-.5 px-1 font-semibold text-xs">
             <span>
               <svg
@@ -21,32 +26,27 @@ export default function Suggestions({ user }) {
               >
                 <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z" />
               </svg>
-              
             </span>
-            75 Posts
+            5 Posts
           </div>
         </div>
         <div className="flex-shrink-0">
           <button className="inline-flex items-center px-3 py-0.5 rounded-full bg-blue-50 text-sm font-medium text-blue-700 hover:bg-blue-100">
             <svg
-              class="-ml-1 mr-0.5 h-5 w-5 text-blue-400"
-              x-description="Heroicon name: solid/plus"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+              width="16"
+              height="16"
               fill="currentColor"
-              aria-hidden="true"
+              class="-ml-1 mr-0.5 h-5 w-5 text-blue-400"
+              viewBox="0 0 16 16"
             >
-              <path
-                fill-rule="evenodd"
-                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                clip-rule="evenodd"
-              ></path>
+              <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
+              <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z" />
             </svg>
-            Follow
+            Visit
           </button>
         </div>
       </div>
-      
     </div>
   );
 }
