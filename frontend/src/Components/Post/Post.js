551519f44ff75cd5af76c8cbea16d5132b152424
bodhisha <bodhisha@gmail.com>
2021-04-09 05:12:28 +0530

    fix likes styles and add link to visit in suggestions

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -126,39 +126,46 @@ export default function Post({ post_initializer }) {
                 {post.author_name}
               </h3>
             </div>
-            <div className="flex gap-x-6">
-              {post.likes.length}
-              <A
-                href="#"
-                onClick={() => {
-                  handleLike(post.post_id);
-                }}
-              >
-                {likesContainID(post.likes, user[0].user_id) ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    fill="red"
-                    className="bi bi-heart-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    fill="currentColor"
-                    className="bi bi-heart"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                  </svg>
+            <div className="flex items-center gap-x-4">
+              <div className="flex gap-x-2 items-center ">
+                <A
+                  href="#"
+                  onClick={() => {
+                    handleLike(post.post_id);
+                  }}
+                >
+                  {likesContainID(post.likes, user[0].user_id) ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      fill="red"
+                      className="bi bi-heart-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      fill="currentColor"
+                      className="bi bi-heart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
+                  )}
+                </A>
+                {post.likes.length > 0 && (
+                  <p>
+                    {post.likes.length}{" "}
+                    {post.likes.length === 1 ? "like" : "likes"}{" "}
+                  </p>
                 )}
-              </A>
+              </div>
               {fade ? (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
--- frontend/src/Components/Suggestions.js
+++ frontend/src/Components/Suggestions.js
@@ -22,7 +22,7 @@ export default function Suggestions({ user }) {
                 xmlns="http://www.w3.org/2000/svg"
                 width="10"
                 fill="currentColor"
-                class="bi bi-image-fill"
+                className="bi bi-image-fill"
                 viewBox="0 0 16 16"
               >
                 <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z" />
@@ -32,20 +32,23 @@ export default function Suggestions({ user }) {
           </div>
         </div>
         <div className="flex-shrink-0">
-          <button className="inline-flex items-center px-3 py-0.5 rounded-full bg-blue-50 text-sm font-medium text-blue-700 hover:bg-blue-100">
+          <A
+            href={`/user/${user.user_id}`}
+            className="inline-flex items-center px-3 py-0.5 rounded-full bg-blue-50 text-sm font-medium text-blue-700 hover:bg-blue-100"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
               height="16"
               fill="currentColor"
-              class="-ml-1 mr-0.5 h-5 w-5 text-blue-400"
+              className="-ml-1 mr-0.5 h-5 w-5 text-blue-400"
               viewBox="0 0 16 16"
             >
               <path d="M4.715 6.542L3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.001 1.001 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z" />
               <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 0 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 0 0-4.243-4.243L6.586 4.672z" />
             </svg>
             Visit
-          </button>
+          </A>
         </div>
       </div>
     </div>
