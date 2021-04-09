d01c26215520f9e0c306ae1202d013943003eaea
bodhisha <bodhisha@gmail.com>
2021-04-10 00:43:01 +0530

    Edit Profile button

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { GET_USER_DETAILS_URL } from "../constants";
 import { Loading } from "../Components/Common/Loader";
 import Post from "./Post/Post";
 import { toast } from "react-toastify";
+import { AuthContext } from "../Context/AuthContext";
 
 export default function UserProfile({ id }) {
-  const [user, setUser] = useState([]);
+  const [userProfile, setUserProfile] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { user } = useContext(AuthContext);
+  const user_id = user[0].user_id;
 
   useEffect(() => {
     setLoading(true);
@@ -15,7 +18,7 @@ export default function UserProfile({ id }) {
       .get(GET_USER_DETAILS_URL + `${id}`)
       .then((res) => {
         setLoading(false);
-        setUser(res.data.data);
+        setUserProfile(res.data.data);
       })
       .catch((err) => {
         setLoading(false);
@@ -35,7 +38,7 @@ export default function UserProfile({ id }) {
               <div className=" items-center">
                 <img
                   className="h-40 w-40 border-black border-2 rounded-full"
-                  src={user.profile_picture}
+                  src={userProfile.profile_picture}
                   alt="Profile_Picture"
                 />
               </div>
@@ -44,28 +47,30 @@ export default function UserProfile({ id }) {
             <div className="flex flex-col w-full md:w-4/5 mx-auto py-2 text-center items-center">
               <div className="text-left pl-4 pt-3 text-center w-2/3">
                 <span className="text-base text-gray-700 text-3xl">
-                  {user.name}
+                  {userProfile.name}
                 </span>
               </div>
 
               <div className="text-left pl-4 pt-3 text-center w-4/5">
                 <p className="text-base font-medium text-black-700 mr-2 box-content text-xl font-mono ">
-                  {user.description}
+                  {userProfile.description}
                 </p>
               </div>
             </div>
 
             <div className="text-left text-center w-1/2 mx-auto">
-              <button className="bg-transparent hover:bg-blue-500 text-gray-700 font-semibold hover:text-white py-2 px-6 border border-gray-600 hover:border-transparent rounded">
-                Edit Profile
-              </button>
+              {userProfile.user_id === user_id && (
+                <button className="bg-transparent hover:bg-blue-500 text-gray-700 font-semibold hover:text-white py-2 px-6 border border-gray-600 hover:border-transparent rounded">
+                  Edit Profile
+                </button>
+              )}
             </div>
           </div>
 
-          {user?.posts && (
+          {userProfile?.posts && (
             <div className="flex flex-col sm:2/3 w-full pt-10 items-start">
               <div className="flex-1 text-center px-4 py-2 m-2 max-w-3xl">
-                {user.posts.map((post) => {
+                {userProfile.posts.map((post) => {
                   return <Post key={post.post_id} post_initializer={post} />;
                 })}
               </div>
