28b4b5747b4933f7d9eef82fa3a14a8647fbded9
Deepak Agrawal <debug.career@gmail.com>
2021-04-16 00:09:11 +0530

    Replaced Delete button with Trash Icon

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -238,12 +238,17 @@ export default function Post({ handleDeletePost, post_initializer }) {
                 </A>
               )}
               {user[0].user_id === post.user_id && (
-                <button
-                  className="bg-red-900 w-auto hover:bg-red-700 text-center text-lg font-normal text-white rounded-md p-1 mt-1"
-                  onClick={() => handleDeletePost(post.post_id)}
-                >
-                  Delete
-                </button>
+                <A href="#" onClick={() => handleDeletePost(post.post_id)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    fill="red"
+                    class="bi bi-trash-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                  </svg>
+                </A>
               )}
             </div>
           </div>
