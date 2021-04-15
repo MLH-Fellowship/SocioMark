b2f6ca8a5ecbf257ed4f2dccfdf3e53e6e7bf01e
Suhani Chawla <suhanichawla2000@gmail.com>
2021-04-15 14:51:55 +0530

    user can edit only their post description

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -302,7 +302,7 @@ export default function Post({ post_initializer }) {
             </div>
           </div>
           <div className="flex justify-between w-full items-center px-4">
-          {!isUserEditingPost ? 
+          {user[0].user_id==post.user_id ? !isUserEditingPost ?
             <>
             {post.description}
             <svg 
@@ -338,6 +338,10 @@ export default function Post({ post_initializer }) {
               <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
               <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
             </svg>
+            </>
+            :
+            <>
+            {post.description}
             </>
           }
           </div>
