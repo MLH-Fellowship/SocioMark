ba016c2316aa6c6486f449aee81cd1c9da6d1917
Suhani Chawla <suhanichawla2000@gmail.com>
2021-04-16 19:34:06 +0530

    formatting using prettier

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -14,7 +14,7 @@ import {
   POST_REPORT_URL,
   POST_REPORT_COOLDOWN,
   POST_VERIFY_URL,
-  POST_EDIT_URL
+  POST_EDIT_URL,
 } from "../../constants";
 
 export default function Post({ post_initializer }) {
@@ -37,32 +37,32 @@ export default function Post({ post_initializer }) {
     setDescription(e.target.value);
   };
 
-  const handleEditPost = (post_id)=>{
+  const handleEditPost = (post_id) => {
     axios
-    .patch(
-      `${POST_EDIT_URL}`,
-      {post_id,description},
-      {
-        headers: {
-          "accept": "application/json",
-          "Authorization": "Bearer " + access,
-        },
-      }
-    )
-    .then((res) => {
-      let new_post = Object.assign({}, post);
-      new_post.description=description
-      setPost(new_post)
-      setUserEditingPost(false);
-      toast.info(JSON.stringify(res.data.message));
-    })
-    .catch(({ response }) => {
-      if (response) {
-        toast.error(JSON.stringify(response.data.detail));
-      }
-      setUserEditingPost(false);
-    });
-  }
+      .patch(
+        `${POST_EDIT_URL}`,
+        { post_id, description },
+        {
+          headers: {
+            accept: "application/json",
+            Authorization: "Bearer " + access,
+          },
+        }
+      )
+      .then((res) => {
+        let new_post = Object.assign({}, post);
+        new_post.description = description;
+        setPost(new_post);
+        setUserEditingPost(false);
+        toast.info(JSON.stringify(res.data.message));
+      })
+      .catch(({ response }) => {
+        if (response) {
+          toast.error(JSON.stringify(response.data.detail));
+        }
+        setUserEditingPost(false);
+      });
+  };
 
   const handleLike = (post_id) => {
     axios
@@ -302,48 +302,50 @@ export default function Post({ post_initializer }) {
             </div>
           </div>
           <div className="flex justify-between w-full items-center px-4">
-          {user[0].user_id==post.user_id ? !isUserEditingPost ?
-            <>
-            {post.description}
-            <svg 
-              onClick={()=>setUserEditingPost(true)}
-              xmlns="http://www.w3.org/2000/svg" 
-              width="25" 
-              height="25" 
-              fill="currentColor" 
-              class="bi bi-pencil-fill clickable" 
-              viewBox="0 0 20 20">
-              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-            </svg>
-            </>
-            :
-            <>
-            <textarea
-              aria-label="description"
-              onChange={handleChange}
-              name="description"
-              value={description}
-              type="description"
-              className="edit-post-area appearance-none block w-full bg-white text-blue-900 font-normal border border-blue-400 rounded py-3 px-4 mb-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              placeholder="Add a description for your post..."
-            />
-            <svg 
-              xmlns="http://www.w3.org/2000/svg" 
-              onClick={()=>handleEditPost(post.post_id)}
-              width="35" 
-              height="35" 
-              fill="green" 
-              class="bi bi-check-circle clickable" 
-              viewBox="0 0 16 16">
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-              <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-            </svg>
-            </>
-            :
-            <>
-            {post.description}
-            </>
-          }
+            {user[0].user_id == post.user_id ? (
+              !isUserEditingPost ? (
+                <>
+                  {post.description}
+                  <svg
+                    onClick={() => setUserEditingPost(true)}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="25"
+                    fill="currentColor"
+                    class="bi bi-pencil-fill clickable"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+                  </svg>
+                </>
+              ) : (
+                <>
+                  <textarea
+                    aria-label="description"
+                    onChange={handleChange}
+                    name="description"
+                    value={description}
+                    type="description"
+                    className="edit-post-area appearance-none block w-full bg-white text-blue-900 font-normal border border-blue-400 rounded py-3 px-4 mb-4 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
+                    placeholder="Add a description for your post..."
+                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => handleEditPost(post.post_id)}
+                    width="35"
+                    height="35"
+                    fill="green"
+                    class="bi bi-check-circle clickable"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
+                  </svg>
+                </>
+              )
+            ) : (
+              <>{post.description}</>
+            )}
           </div>
           {post.comments.map((comment) => {
             return (
